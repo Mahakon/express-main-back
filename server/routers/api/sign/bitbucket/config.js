62edd08be76d14c46fc279b933e8fc6edfc2ce9e
Mariia Kononenko <mkon.masha@gmail.com>
2018-04-22 12:43:40 +0300

    change hosts

--- server/routers/api/sign/bitbucket/config.js
+++ server/routers/api/sign/bitbucket/config.js
@@ -1,7 +1,7 @@
 const config = {
   key: 'HeXa6xKMQHt9DXd6bk',
   secret: 'wQp9cqunGCfyXPcSnYW9VqYftUxnQ2gw',
-  host: 'https://bot-tasker.info',
+  host: 'https://dd171f26.ngrok.io',
   parseServerHost: 'http://127.0.0.1:3001/?'
 };
 
